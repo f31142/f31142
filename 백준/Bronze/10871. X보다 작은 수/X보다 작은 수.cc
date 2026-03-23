04c33d634ac0839204fc50784266cbf9bf1fff63
f31142 <f31142420@gmail.com>
2026-03-24 01:01:38 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, x;
+	int a;
+	int arr[10000];
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		arr[i] = a;
+	}
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < x) {
+			cout << arr[i] << " ";
+		}
+	}
+	return 0;
+}
