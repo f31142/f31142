0786c563889170cf259058ebc42cf9e7dacdd03f
f31142 <f31142420@gmail.com>
2026-03-23 23:38:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = n-i; j > 1; j--) {
+			cout << " ";
+		}
+		for (int k = n+i+1; k > n; k--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
